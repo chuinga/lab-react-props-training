d7411573f6b82d060332c4d349f0bc1fbc885928
Miguel Martins <mfproencamartins@gmail.com>
2023-12-11 15:36:51 +0000

    starting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from 'react';
+import IdCard from './components/IdCard.jsx'
 import "./App.css";
 
-function App() {
+function App() { 
   return (
-    <div className="App">
-      <h1> LAB | React Training</h1>
-    </div>
-  );
+  <div className="App">
+    <IdCard
+  lastName='Doe'
+  firstName='John'
+  gender='male'
+  height={178}
+  birth={new Date("1992-07-14")}
+  picture="https://randomuser.me/api/portraits/men/44.jpg"
+/>
+
+<IdCard
+  lastName='Delores '
+  firstName='Obrien'
+  gender='female'
+  height={172}
+  birth={new Date("1988-05-11")}
+  picture="https://randomuser.me/api/portraits/women/44.jpg"
+/>  
+  </div>  
+  )
 }
+
 
 export default App;
---
+++ src/components/IdCard.jsx
@@ -0,0 +1,14 @@
+function IdCard (props) {
+    return (
+        <div className="IdCard">
+            <p>lastName</p>
+            <p>firstName</p>
+            <p>gender</p>
+            <p>height</p>
+            <p>birth</p>
+            <p>picture</p>
+        </div>
+    )
+}
+
+export default IdCard;
